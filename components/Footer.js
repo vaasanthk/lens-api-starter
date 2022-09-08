142df69376c80d245bbbbf1ec7d7c45a7d76f951
Vasanth kumar <vasanth.3656@yahoo.com>
2022-09-09 02:02:40 +0530

    made changes to index.js, api.js and addres abi.json file

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from "next/link"
 
 export default function Footer() {
   return (
@@ -49,5 +49,5 @@ export default function Footer() {
         </div>
       </div>
     </footer>
-  );
+  )
 }
